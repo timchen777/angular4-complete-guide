d5944223f49f7c494d0a3511c29f085323271f51
timchen777 <timchen7@yahoo.com>
2017-04-05 10:55:44 +0800

    from udemy angular4 complete guide

---
+++ README.rb
@@ -0,0 +1,5 @@
+================== https://www.udemy.com/the-complete-guide-to-angular-2/learn/==============================
+> npm install -g angular-cli @angular/cli 
+> ng new a4cg
+> cd a4cg
+> ng serve ==> localhist:4200 see App Works! 
